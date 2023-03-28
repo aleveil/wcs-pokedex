bbf1b96488923a91630c277503d716e9e0c99aa3
aleveil <anthony.leveil@protonmail.com>
2023-03-28 15:45:18 +0200

    ex 04

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,4 @@
-const pokemonList = [
-	{
-	  name: "bulbasaur",
-	  imgSrc:
-		"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-	},
-	{
-	  name: "mew",
-	},
-  ];
-
-function PokemonCard() {
-	let pokemon = pokemonList[0];
+function PokemonCard({pokemon}) {
 	return (
 		<figure>
 			{pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>}
